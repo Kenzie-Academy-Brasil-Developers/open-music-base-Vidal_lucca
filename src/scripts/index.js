1a93e1156bc7a039a013f776f4d0444ef08e7834
Lucca Vidal <129102372+VidalLucca@users.noreply.github.com>
2023-07-05 18:56:10 -0300

    atualizado

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,36 +1,36 @@
 // /* Desenvolva sua lógica aqui ... */
 function filterByCategory(category) {
   if (category === 0) {
-  renderCards(products);
+    renderCards(products);
   } else {
-  const filteredProducts = products.filter(
-  (product) => product.category === category
-  );
-  renderCards(filteredProducts);
+    const filteredProducts = products.filter(
+      (product) => product.category === category
+    );
+    renderCards(filteredProducts);
   }
-  }
-  
-  buttons.forEach((button, index) => {
+}
+
+buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
-  const selectedCategory = index;
-  filterByCategory(selectedCategory);
+    const selectedCategory = index;
+    filterByCategory(selectedCategory);
   });
-  });
-  
-  inputPrice.addEventListener("input", function (event) {
+});
+
+inputPrice.addEventListener("input", function (event) {
   const newPrice = parseFloat(event.target.value);
   selectedPrice.textContent = `Até R$ ${newPrice
-  .toFixed(2)
-  .replace(".", ",")}`;
-  
+    .toFixed(2)
+    .replace(".", ",")}`;
+
   const filteredProducts = products.filter(
-  (product) => product.price <= newPrice
+    (product) => product.price <= newPrice
   );
   renderCards(filteredProducts);
-  });
-  
-  // Initial rendering of cards with the products array
-  renderCards(products);
+});
+
+// Initial rendering of cards with the products array
+renderCards(products);
 
 
 
